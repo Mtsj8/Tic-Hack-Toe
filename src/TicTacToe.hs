138c3d731d836b7be2c16ea50289c247e47fa291
Mtsj8 <marcelojunior.tranche@gmail.com>
2022-08-17 21:19:39 -0300

    Ramdomizando movimentos da IA

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -3,6 +3,7 @@ module TicTacToe where
 {-# LANGUAGE FlexibleInstances #-}
 
 
+import System.Random
 import Data.Either
 import Safe
 import Data.Maybe
@@ -10,7 +11,7 @@ import Data.List
 
 -- |Slots in the board can either be filled with Naughts or Crosses
 data Symbol = X | O
-	deriving (Show, Eq)
+    deriving (Show, Eq)
 
 -- |Empty slots are referred to by their location on the board
 type Piece = Either Int Symbol
@@ -20,17 +21,17 @@ type Three = [Piece] -- how do I constrain this to a length of three?
 
 -- |The game board is made up of three rows of three pieces each.
 data Board = Board Three Three Three
-	deriving (Eq)
+    deriving (Eq)
 
 --Thank you to http://projects.haskell.org/operational/examples/TicTacToe.hs.html for the show function
 instance Show Board where
-	show board =
-	      unlines . surround "+---+---+---+"
-	    . map (concat . surround "|". map showSquare)
-	    $ (rows board)
-	    where
-	    surround x xs = [x] ++ intersperse x xs ++ [x]
-	    showSquare = either (\n -> " " ++ show n ++ " ") (\n -> color n)
+    show board =
+        unlines . surround "+---+---+---+"
+        . map (concat . surround "|". map showSquare)
+        $ (rows board)
+        where
+        surround x xs = [x] ++ intersperse x xs ++ [x]
+        showSquare = either (\n -> " " ++ show n ++ " ") (\n -> color n)
 
 -- | \ESC[ for ANSI escape
 esc :: Int -> String
@@ -49,9 +50,9 @@ emptyBoard = Board [Left 1, Left 2, Left 3] [Left 4, Left 5, Left 6] [Left 7, Le
 -- |Given either a row, column, or diagonal, it checks whether it is entirely filled with naughts or crosses
 full :: Three -> Bool
 full ts@[a,b,c] = noLefts && allEqual
-	where
-		noLefts = foldl (\acc curr -> acc && (isRight curr)) True ts
-		allEqual = a == b && b == c
+    where
+        noLefts = foldl (\acc curr -> acc && (isRight curr)) True ts
+        allEqual = a == b && b == c
 
 -- |Given a game board, check whether the game is over because someone won
 won :: Board -> Bool
@@ -64,9 +65,9 @@ draw b = length (possibleMoves b) == 0
 -- |Message to display to the user about the results of the game
 winner :: Board -> String
 winner b = if length winnerType > 0 then head winnerType else "It was a draw!"
-	where
-		allConfigs = ((rows b) ++ (cols b) ++ (diags b))
-		winnerType = [if a == (Right O) then "The computer wins!" else "You win!" | curr@[a,b,c] <- allConfigs, full curr]
+    where
+        allConfigs = ((rows b) ++ (cols b) ++ (diags b))
+        winnerType = [if a == (Right O) then "The computer wins!" else "You win!" | curr@[a,b,c] <- allConfigs, full curr]
 
 -- |Extract rows from game board
 rows :: Board -> [Three]
@@ -94,37 +95,22 @@ listToBoard [a,b,c,d,e,f,g,h,i] = Board [a,b,c] [d,e,f] [g,h,i]
 
 -- |Function to update the game board with a new value at a specified point
 findAndReplace :: Board -> Piece -> Piece -> Board
-findAndReplace b p1 p2 = listToBoard [if x==p1 then p2 else x | x <- bl]
-	where bl = boardToList b
+findAndReplace board p1 p2 = listToBoard [if x==p1 then p2 else x | x <- bl]
+    where bl = boardToList board
 
--- |Check if O's can immediately win, and if so, do it
-winningOMove :: Board -> Maybe Board
-winningOMove b = headMay [findAndReplace b p (Right O) | p <- (possibleMoves b), won (findAndReplace b p (Right O))]
+seed::Int
+seed = 42
+generator = mkStdGen seed
 
--- |Check if X's can immediately win, and if so, block it
-blockXWin :: Board -> Maybe Board
-blockXWin b = headMay [findAndReplace b p (Right O) | p <- (possibleMoves b), won (findAndReplace b p (Right X))]
-
--- |Check whether a board has been forked
-isFork :: Board -> Bool
-isFork b = 2 == length [findAndReplace b p (Right O) | p <- (possibleMoves b), won (findAndReplace b p (Right O))]
-
--- |Check if O's can make a fork, and if so, do it
-forkO :: Board -> Maybe Board
-forkO b = headMay [findAndReplace b p (Right O) | p <- (possibleMoves b), isFork (findAndReplace b p (Right O))]
-
--- |Check if X's can make a fork, and if so, block it
-blockXFork :: Board -> Maybe Board
-blockXFork b = headMay [findAndReplace b p (Right O) | p <- (possibleMoves b), isFork (findAndReplace b p (Right X))]
+getRandomElement :: [Piece] -> Piece
+getRandomElement pieces = pieces !! rand where
+    n = length pieces
+    (rand, _) = randomR (0,(n-1)) generator
 
 -- |Decision tree for AI that will go down the list to make its move
 makeOMove :: Board -> Board
 makeOMove board@(Board x@[a, b, c] y@[d, e, f] z@[g, h, i])
-	| isJust (winningOMove board) 	= fromJust (winningOMove board)
-	| isJust (blockXWin board) 		= fromJust (blockXWin board)
-	| isJust (forkO board) 			= fromJust (forkO board)
-	| isJust (blockXFork board) 	= fromJust (blockXFork board)
 	| elem e (possibleMoves board)	= findAndReplace board e (Right O)
 	| otherwise 					= if length (possibleMoves board) > 0
-		then findAndReplace board (head (possibleMoves board)) (Right O)
+		then findAndReplace board (getRandomElement (possibleMoves board)) (Right O)
 		else board --This should not happen
